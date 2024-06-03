a528a20e3d3b9c007655cc09fc2ac673ca20bf5b
Pujan Raj Rai <70184988+pujanrajrai@users.noreply.github.com>
2024-06-03 15:03:02 +0545

    uncommented urls of rental

--- apps/rental/urls.py
+++ apps/rental/urls.py
@@ -3,11 +3,11 @@ from . import views
 app_name = "rental"
 
 urlpatterns = [
-    # path('customer/', include('rental.pages.customer.urls')),
-    # path('property/', include('rental.pages.property.urls')),
-    # path('customer_property/', include('rental.pages.customer_property.urls')),
-    # path('invoice/', include('rental.pages.invoice.urls')),
-    # path('ledger/', include('rental.pages.ledger.urls')),
-    # path('payment/', include('rental.pages.payment.urls')),
+    path('customer/', include('rental.pages.customer.urls')),
+    path('property/', include('rental.pages.property.urls')),
+    path('customer_property/', include('rental.pages.customer_property.urls')),
+    path('invoice/', include('rental.pages.invoice.urls')),
+    path('ledger/', include('rental.pages.ledger.urls')),
+    path('payment/', include('rental.pages.payment.urls')),
 
 ]
